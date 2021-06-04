dea9351fd3422e2683227e91b993004a4154aa3d
Fabian Castillo <efcastillo81@misena.edu.co>
2021-06-04 10:50:36 -0500

    Prueba portafolio

---
+++ js/script.js
@@ -0,0 +1,20 @@
+const typed = new Typed('.typed', {
+    strings: [
+        'ILUSTRACIÓN',
+        'EDICIÓN',
+        'PUBLICIDAD',
+        'DISEÑO',
+        'CREATIVIDAD',
+        'CUMPLIMIENTO'
+    ],
+    //Configuraciones//
+    typeSpeed: 75,
+    startDelay: 200,
+    backSpeed: 75,
+    backDelay: 1500,
+    loop: true,
+    loopCount: false,
+    cursorChar: '|',
+    contentType: 'html',
+
+});
